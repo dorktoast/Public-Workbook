04393de6c4114c331c9346037deb180f34ea0628
Sam Swicegood <60187671+dorktoast@users.noreply.github.com>
2023-10-08 20:47:12 -0600

    Create AdobeKiller.bat
    adobekiller

---
+++ App Script/AdobeKiller.bat
@@ -0,0 +1,16 @@
+@echo off
+title Adobe Killer
+echo You are about to terminate all Adobe processes and services. Please close all Adobe programs before proceeding.
+pause
+
+net stop AdobeUpdateService /y
+taskkill /IM AdobeUpdateService.exe /F
+taskkill /IM "Adobe Installer.exe" /F
+taskkill /IM "Adobe Desktop Service.exe" /F
+taskkill /IM "AdobeNotificationClient.exe" /F
+taskkill /IM "AdobeIPCBroker.exe" /F
+taskkill /IM "Adobe CEF Helper.exe" /F
+taskkill /IM CCLibrary.exe /F
+taskkill /IM CCXProcess.exe /F
+taskkill /IM CoreSync.exe /F
+taskkill /IM "Adobe Crash Processor.exe" /F
