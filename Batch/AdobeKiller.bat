5d764550a183df60359e4fbdca64ec74b5ea378a
Sam Swicegood <60187671+dorktoast@users.noreply.github.com>
2023-10-23 13:17:07 -0600

    Update AdobeKiller.bat
    Added additional process names

--- Batch/AdobeKiller.bat
+++ Batch/AdobeKiller.bat
@@ -8,9 +8,13 @@ taskkill /IM AdobeUpdateService.exe /F
 taskkill /IM "Adobe Installer.exe" /F
 taskkill /IM "Adobe Desktop Service.exe" /F
 taskkill /IM "AdobeNotificationClient.exe" /F
+taskkill /IM "AcrobatNotificationClient.exe" /F
 taskkill /IM "AdobeIPCBroker.exe" /F
 taskkill /IM "Adobe CEF Helper.exe" /F
 taskkill /IM CCLibrary.exe /F
+taskkill /IM armsvc.exe /F
+taskkill /IM AGMService.exe /F
+taskkill /IM AdobeCollabSync.exe /F
 taskkill /IM CCXProcess.exe /F
 taskkill /IM CoreSync.exe /F
 taskkill /IM "Adobe Crash Processor.exe" /F
