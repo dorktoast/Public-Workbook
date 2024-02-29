762109ae84c015c3a473723a772e1bebee81acbe
Sam Swicegood <60187671+dorktoast@users.noreply.github.com>
2024-02-28 18:48:20 -0700

    Update AdobeKiller.bat
    added iterations and updated processes

--- Batch/AdobeKiller.bat
+++ Batch/AdobeKiller.bat
@@ -2,7 +2,7 @@
 title Adobe Killer
 echo You are about to terminate all Adobe processes and services. Please close all Adobe programs before proceeding.
 pause
-
+echo Iteration 1
 net stop AdobeUpdateService /y
 taskkill /IM AdobeUpdateService.exe /F
 taskkill /IM "Adobe Installer.exe" /F
@@ -11,6 +11,25 @@ taskkill /IM "AdobeNotificationClient.exe" /F
 taskkill /IM "AcrobatNotificationClient.exe" /F
 taskkill /IM "AdobeIPCBroker.exe" /F
 taskkill /IM "Adobe CEF Helper.exe" /F
+taskkill /IM "Adobe Crash Processor.exe" /F
+taskkill /IM "Creative Cloud UI Helper.exe" /F
+taskkill /IM CCLibrary.exe /F
+taskkill /IM armsvc.exe /F
+taskkill /IM AGMService.exe /F
+taskkill /IM AdobeCollabSync.exe /F
+taskkill /IM CCXProcess.exe /F
+taskkill /IM CoreSync.exe /F
+taskkill /IM "Adobe Crash Processor.exe" /F
+echo Iteration 2
+taskkill /IM AdobeUpdateService.exe /F
+taskkill /IM "Adobe Installer.exe" /F
+taskkill /IM "Adobe Desktop Service.exe" /F
+taskkill /IM "AdobeNotificationClient.exe" /F
+taskkill /IM "AcrobatNotificationClient.exe" /F
+taskkill /IM "AdobeIPCBroker.exe" /F
+taskkill /IM "Adobe CEF Helper.exe" /F
+taskkill /IM "Adobe Crash Processor.exe" /F
+taskkill /IM "Creative Cloud UI Helper.exe" /F
 taskkill /IM CCLibrary.exe /F
 taskkill /IM armsvc.exe /F
 taskkill /IM AGMService.exe /F
